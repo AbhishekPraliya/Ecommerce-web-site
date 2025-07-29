55f6a67da122ff1903e2d654a27fe92c04b39946
AbhishekPraliya <168660055+AbhishekPraliya@users.noreply.github.com>
2025-07-29 21:38:25 +0530

    port update
    port update

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ const __dirname = path.dirname(__filename);
 const app=express();
 
 dotenv.config();
-const PORT = process.env.PORT || 5001;
+const PORT = Number(process.env.PORT) || 5001;
 
 
 app.use(express.json());
